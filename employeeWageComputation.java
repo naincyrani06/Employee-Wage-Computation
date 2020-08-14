de440f234d6c687c9bc384a1b092779fa6671f86
naincyrani06 <naincyrani06it@gmail.com>
2020-08-14 23:26:22 +0530

    Displaying whether the Employee is Present or Absent

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -2,6 +2,19 @@ public class employeeWageComputation
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee-Wage-Computation Program");
+		System.out.println("Welcome to Employee Wage Computation Problem in Java");
+		int isPresent=1;
+
+        	int empCheck=(int)((Math.random()*10)%2);
+
+        	if(isPresent==empCheck)
+        	{
+            		System.out.println("Employee is Present");
+       		}
+        	else
+        	{
+            		System.out.println("Employee is Absent");
+        	}
 	}
 }
+
