04417c36089e2ccef49aa982db585fd784c65b53
naincyrani06 <naincyrani06it@gmail.com>
2020-08-15 00:29:41 +0530

    Adding Part Time Employee Wage To Daily Employee Wage

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -3,16 +3,21 @@ public class employeeWageComputation
 	public static void main(String[] args)
 	{
 		int fullTimeEmp=1;
-        	int empRatePerHour=20;
+		int partTime=2;
+		int empRatePerHour=20;
 		int empHrs = 0;
-        	int empWage=0;
-        	int randomCheck=(int)((Math.random()*10)%2);
-        	if(fullTimeEmp==randomCheck)
+		int empWage=0;
+		int randomCheck=(int)((Math.random()*10)%3);
+		if(fullTimeEmp==randomCheck)
 		{
 			empHrs = 8;
 		}
-		empWage=empHrs*empRatePerHour;
-		System.out.println("Employee Wage is : " + empWage);
+		else if (partTime==randomCheck)
+		{
+			empHrs = 4;
+		}
+		empWage =empHrs*empRatePerHour;
+		System.out.println("Employee Wage: " + empWage);
 	}
 }
 
